255b39054642af6342d5a3e538dafeca209c33b8
liulei237316 <liulei237136@163.com>
2021-02-14 02:35:34 +0000

    patch

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
             'driver'   => 'pgsql',
             'host'     => $db_config['host'],
             // 'port'     => env('DB_PORT', '5432'),
-            'port'     => $db_config('port', '5432'),
+            'port'     => $db_config['port'] || '5432',
             'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
