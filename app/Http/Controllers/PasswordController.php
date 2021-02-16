f88aef591ab60dc83c4a0e2c527452f80551cfd0
liulei237316 <liulei237136@163.com>
2021-02-16 15:08:49 +0000

    重置密码

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -50,4 +50,59 @@ class PasswordController extends Controller
         session()->flash('success', '重置邮件发送成功，请查收');
         return redirect()->back();
     }
+
+    public function showResetForm(Request $request){
+        $token = $request->route()->parameter('token');
+        return view('auth.passwords.reset', compact('token'));
+    }
+
+    public function reset(Request $request){
+        //1. 验证数据是否合规
+        $this->validate($request, [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:8',
+        ]);
+        $email = $request->email;
+        $token = $request->token;
+        //找回密码链接的有效时间 10分钟
+        $expires = 60 * 10;
+
+        // 2.获取用户对象
+        $user = User::where('email', $email)->first();
+
+        // 3. 如果不存在
+        if (is_null($user)) {
+            session()->flash('danger', '邮箱未注册');
+            return redirect()->back()->withInput();
+        }
+
+        //4.读取重置的记录
+        $record = (array)DB::table('password_resets')->where('email', $email)->first();
+
+        if (!$record) {
+            session()->flash('danger', '未找到重置记录');
+            return redirect()->back();
+        }
+
+        //5.检查是否过期
+        if (Carbon::parse($record['created_at'])->addSecond($expires)->isPast()){
+            session()->flash('danger', '链接已过期，请重新尝试');
+            return redirect()->back();
+        }
+
+        //6.检查是否正确
+        if (! Hash::check($token, $record['token'])) {
+            session()->flash('danger', '令牌错误');
+            return redirect()->back();
+        }
+
+        //7. 一切正常，更新用户密码
+        $user->update(['password' => bcrypt($request->password)]);
+
+        //8.提示用户更新成功
+        session()->flash('success', '密码重置成功，请使用新密码登录');
+        return redirect()->route('login');
+
+    }
 }
