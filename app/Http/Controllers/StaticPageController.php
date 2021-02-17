5b0b7ae449d3e3b385a83e915daf5850b5602b0b
liulei237316 <liulei237136@163.com>
2021-02-17 01:28:05 +0000

    首页微博列表

--- app/Http/Controllers/StaticPageController.php
+++ app/Http/Controllers/StaticPageController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class StaticPageController extends Controller
 {
     public function home(){
-        return view('static_pages/home');
+        $feed_items = [];
+        if(Auth::check()){
+            $feed_items = Auth::user()->statuses()->paginate(30);
+        }
+        return view('static_pages/home', compact('feed_items'));
     }
     public function about(){
         return view('static_pages/about');
