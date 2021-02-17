10960d5a2cd922fc6fe87c910e2d5fe34824554a
liulei237316 <liulei237136@163.com>
2021-02-17 15:08:41 +0000

    社交统计数据

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         Model::unguard();
+
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
+
         Model::reguard();
     }
 }
